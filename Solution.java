2cea0991607c32e025a5cda62e89123b386b61ad
sdim-ua <ingersol@yandex.ua>
2019-01-29 16:00:59 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,29 @@
+/* 
+Создать класс Dog
+*/
+
+public class Dog {
+    //напишите тут ваш код
+    String name;
+    int height; 
+    String color;
+    
+    public void initialize (String name){
+        this.name = name;
+    }
+    
+    public void initialize (String name, int height){
+        this.name = name;
+        this.height = height;
+    }
+    
+    public void initialize (String name, int height, String color){
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
